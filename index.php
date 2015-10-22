bd2f86f14de20bc190d497b48ffc253dd1d90b9f
Mattias Wikström <mw222rs@student.lnu.se>
2015-10-22 15:34:03 +0200

    test [deploy: committr]

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 require_once("AppSettings.php");
 require_once("vendor/autoload.php");
 
+if (\AppSettings::DEVELOPING) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'On');
+}
+
 $db = new Committr\Model\DAL\MongoDAL();
 
 $layout = new Committr\View\LayoutView();
