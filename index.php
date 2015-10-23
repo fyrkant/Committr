1e21085f58661e62d902836705c432b05f9e7b5c
Mattias <mw222rs@student.lnu.se>
2015-10-23 14:48:57 +0200

    macn it up

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once("AppSettings.php");
 require_once("vendor/autoload.php");
 
-if (\AppSettings::DEVELOPING) {
+if (\AppSettings::DEBUGGING) {
     error_reporting(E_ALL);
     ini_set('display_errors', 'On');
 }
