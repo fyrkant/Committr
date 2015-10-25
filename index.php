d63342bfd9fae253aa871c3e73a542f2fd0151fd
Mattias Wikström <mw222rs@student.lnu.se>
2015-10-25 13:59:43 +0100

    curling

--- index.php
+++ index.php
@@ -13,4 +13,10 @@ $db = new Committr\Model\DAL\MongoDAL();
 $layout = new Committr\View\LayoutView();
 $layout->renderPage();
 
+$data = new \Committr\Model\GetGithubData();
+$dataTest = $data->getContentFromGithub('https://api.github.com/users/mw222rs/repos');
 
+
+
+
+var_dump($data->getContentFromGithub('https://api.github.com/repos/mw222rs/committr/commits'));
---
+++ src/Model/GetGithubData.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fyrkant
+ * Date: 2015-10-25
+ * Time: 11:44
+ */
+
+namespace Committr\Model;
+
+
+class GetGithubData
+{
+
+    public function getContentFromGithub($url)
+    {
+        try {
+            $curlHandle = curl_init();
+
+            if ($curlHandle === false) {
+                throw new \Exception("Failed to init cURL");
+            }
+
+            curl_setopt($curlHandle, CURLOPT_URL, $url);
+            curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curlHandle, CURLOPT_USERAGENT, "committr");
+//            curl_setopt($curlHandle, CURLOPT_CONNECTTIMEOUT, 1);
+            $content = curl_exec($curlHandle);
+
+            if ($content === false) {
+                throw new \Exception(curl_error($curlHandle), curl_errno($curlHandle));
+            }
+            curl_close($curlHandle);
+        } catch (\Exception $e) {
+
+            trigger_error(sprintf('cURL failed with error #%d: %s',
+                $e->getCode(), $e->getMessage()),
+                E_USER_ERROR);
+
+        }
+
+
+        return $content;
+
+    }
+
+    public function test()
+    {
+        $plugin = "overlay";
+        $pathToCache = $_SERVER['DOCUMENT_ROOT'] . '/plugin-cache/';
+        $fileCache = $plugin . '-github.txt';
+        $githubJSON = $this->getRepositoryJSONData($pathToCache . $fileCache, $plugin);
+
+
+    }
+
+    public function getRepositoryJSONData($file, $plugin)
+    {
+        $currrentTime = time();
+        $expireTime = 24 * 60 * 60;
+        $fileTime = filemtime($file);
+
+        if (file_exists($file) && ($currrentTime - $expireTime < $fileTime)) {
+            return json_decode(file_get_contents($file));
+        } else {
+           $JSON = array();
+
+        }
+    }
+
+
+}
